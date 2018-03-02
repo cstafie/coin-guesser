d9824f92acf6d1645376e255606e0301c002ca8d
cristian <stafie.cristian@gmail.com>
2018-03-02 13:59:08 -0500

    fill board and make hash from coins and powerset

--- coin-guesser.rkt
+++ coin-guesser.rkt
@@ -14,8 +14,22 @@
 (define SQUARE-SIZE 50)
 (define COIN-SIZE 15)
 (define SQUARE-COLOR1 (color 200 200 200))
+;(define SQUARE-COLOR1 "purple")
 (define SQUARE-COLOR2 (color 50 50 50))
-(define COIN-COLOR "gold")
+(define COIN-HEADS-COLOR "gold")
+;(define COIN-TAILS-COLOR "silver")
+(define COIN-TAILS-COLOR (color 0 0 0 0))
+(define POWER-SET-SIZE (inexact->exact (log (sqr BOARD-SIZE) 2)))
+
+(define BLANK-SQUARE (square SQUARE-SIZE "solid" (color 0 0 0 0)))
+(define HEADS (freeze (place-image
+                       (circle COIN-SIZE "solid" COIN-HEADS-COLOR)
+                       (/ SQUARE-SIZE 2) (/ SQUARE-SIZE 2)
+                       BLANK-SQUARE)))
+(define TAILS (freeze (place-image
+                       (circle COIN-SIZE "solid" COIN-TAILS-COLOR)
+                       (/ SQUARE-SIZE 2) (/ SQUARE-SIZE 2)
+                       BLANK-SQUARE)))
 
 (define (scalev v) (* SQUARE-SIZE v))
 (define (get-x position) (modulo position BOARD-SIZE))
@@ -26,17 +40,29 @@
   (if (equal? (modulo (+ (car position) (cdr position)) 2) 0)
       SQUARE-COLOR1 SQUARE-COLOR2))
 
+(define (make-coin heads)
+  (freeze (place-image
+           (circle COIN-SIZE "solid" (if heads COIN-HEADS-COLOR COIN-TAILS-COLOR))
+           (/ SQUARE-SIZE 2) (/ SQUARE-SIZE 2)
+           BLANK-SQUARE)))
+
+(define (flip-coins n)
+  (cond [(zero? n) '()]
+        [else (cons (random 2) (flip-coins (sub1 n)))]))
+
 (define (draw-image-on-board img position board)
   (place-image/align img (scalev (car position)) (scalev (cdr position)) "left" "top" board))
 
-(define (draw-board)
+(define (draw-board size)
   (define (draw-squares counter img)
-    (cond [(equal? counter (sqr BOARD-SIZE)) img]
+    (cond [(equal? counter (sqr size)) (freeze img)]
           [else
            (let ([position (get-xy counter)])
-             (draw-squares (add1 counter) (draw-image-on-board
-                                           (square SQUARE-SIZE "solid" (square-color position)) position img)))]))
-  (draw-squares 0 (empty-scene (scalev BOARD-SIZE) (scalev BOARD-SIZE))))
+             (draw-squares
+              (add1 counter)
+              (draw-image-on-board
+               (square SQUARE-SIZE "solid" (square-color position)) position img)))]))
+  (draw-squares 0 (empty-scene (scalev size) (scalev size))))
 
 (define (powerset s)
   (cond [(empty? s) '(())]
@@ -44,14 +70,34 @@
                 (append rest-powerset
                         (map (λ (l) (cons (first s) l)) rest-powerset)))]))
 
-(define EMPTY-BOARD (freeze (draw-board)))
-(define COIN (freeze (place-image
-                      (circle COIN-SIZE "solid" COIN-COLOR)
-                      (/ SQUARE-SIZE 2) (/ SQUARE-SIZE 2)
-                      (square SQUARE-SIZE "solid" (color 0 0 0 0)))))
+(define (fill-board counter coins board)
+  (cond [(empty? coins) (freeze board)]
+        [else (fill-board (add1 counter) (rest coins)
+                          (draw-image-on-board (make-coin (zero? (first coins))) (get-xy counter) board))]))
+;(fill-board-helper 0 (flip-coins (sqr size)) EMPTY-BOARD))
 
 
+(define (hash-addset h s)
+  (println h)
+  (cond [(empty? s) h]
+        [(hash-has-key? h (first s))
+         (hash-addset (hash-set h (first s) (add1 (hash-ref h (first s)))) (rest s))]
+        [else (hash-addset (hash-set h (first s) 1) (rest s))]))
 
+(define (build-hash coins ps h)
+  (cond [(empty? coins) h]
+        [(zero? (first coins)) (build-hash (rest coins) (rest ps) (hash-addset h (first ps)))]
+        [else (build-hash (rest coins) (rest ps) h)]))
+
+
+(define EMPTY-BOARD (draw-board BOARD-SIZE))
+(define COINS (flip-coins (sqr BOARD-SIZE)))
+(define PS (powerset (build-list POWER-SET-SIZE identity)))
+
+(fill-board 0 COINS EMPTY-BOARD)
+COINS
+PS
+(build-hash COINS PS (make-immutable-hash))
 
     
 
